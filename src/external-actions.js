e53bf41996a2183b3eb8b4ebe328976ee49c621c
Hugo Grochau <hugo@grochau.com>
2017-02-20 13:35:02 -0300

    Remove debug console.log

--- src/external-actions.js
+++ src/external-actions.js
@@ -3,14 +3,13 @@ import omit from 'lodash/omit';
 // pulls player info from tracker and updates through the api
 export const updatePlayer = (player, tracker, internalApi) => new Promise((resolve, reject) =>
     tracker.api(player.platform, player.id)
-      .then((stats) => {
-        console.log(stats);
-        return internalApi.player.update({
+      .then((stats) =>
+        internalApi.player.update({
           platform: player.platform,
           id: player.id,
           body: omit(stats.player, ['id', 'platform']),
-        });
-      })
+        })
+      )
       .then((info) => {
         if (info.data.updated) {
           return resolve(`Updated player: ${player.name}`);
